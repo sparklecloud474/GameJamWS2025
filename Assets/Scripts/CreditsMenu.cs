09099586903825b83ff53a377e9e6cdaf68cc310
sparklecloud474 <l.drueckhamer@gmail.com>
2025-12-09 00:55:42 +0100

    Credit Buttons can now open the itch io links

--- Assets/Scripts/CreditsMenu.cs
+++ Assets/Scripts/CreditsMenu.cs
@@ -7,14 +7,68 @@ public class CreditsMenu : MonoBehaviour
 
     [SerializeField] private Button BackButton;
 
+    [SerializeField] private Button MaxButton;
+    [SerializeField] private Button NancyButton;
+    [SerializeField] private Button DorinButton;
+    [SerializeField] private Button ErikButton;
+    [SerializeField] private Button SherryButton;
+    [SerializeField] private Button LeaButton;
+    [SerializeField] private Button LeniButton;
+    [SerializeField] private Button MiaButton;
+
+
     void Start()
     {
         BackButton.onClick.AddListener(OnBackClicked);
+        MaxButton.onClick.AddListener(OnMaxClicked);
+        NancyButton.onClick.AddListener(OnNancyClicked);
+        DorinButton.onClick.AddListener(OnDorinClicked);
+        ErikButton.onClick.AddListener(OnErikClicked);
+        SherryButton.onClick.AddListener(OnSherryClicked);
+        LeaButton.onClick.AddListener(OnLeaClicked);
+        LeniButton.onClick.AddListener(OnLeniClicked);
+        MiaButton.onClick.AddListener(OnMiaClicked);
     }
 
     private void OnBackClicked()
     {
         gameObject.SetActive(false);
         MainMenu.SetActive(true);
+    }
+
+    private void OnMaxClicked()
+    {
+        Application.OpenURL("https://holymakarony.itch.io");
+    }
+
+    private void OnNancyClicked()
+    { 
+        Application.OpenURL("https://sparklecloud474.itch.io");
+    }
+
+    private void OnDorinClicked()
+    {
+
+    }
+
+    private void OnErikClicked()
+    {
+
+    }
+    private void OnSherryClicked()
+    {
+
+    }
+    private void OnLeaClicked()
+    {
+
+    }
+    private void OnLeniClicked()
+    {
+
+    }
+    private void OnMiaClicked()
+    {
+
     }
 }
