8722441b03c329550f0755e86a462a1b9f7cde7f
sparklecloud474 <l.drueckhamer@gmail.com>
2025-12-11 13:36:39 +0100

    Audio Slider fixed

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -35,32 +35,33 @@ public class OptionsMenu : MonoBehaviour
         musicSlider.onValueChanged.AddListener(SetMusicVolume);
     }
 
-    void ApplyVolume(float master, float sfx, float music)
+    private void Update()
     {
-        mixer.SetFloat("MasterVolume", LinearToDb(master));
-        mixer.SetFloat("SFXVolume", LinearToDb(-master));
-        mixer.SetFloat("MusicVolume", LinearToDb(-master));
+        //float db;
+        //mixer.GetFloat("MusicVolume", out db);
+        //Debug.Log("Music db: " + db);
     }
 
-    float LinearToDb(float linear)
+    void ApplyVolume(float master, float sfx, float music)
     {
-        if (linear <= 0.0001f) return -80f;
-        return Mathf.Log10(Mathf.Clamp(linear, 0.0001f, 1f)) * 20f;
+        mixer.SetFloat("MasterVolume", (master));
+        mixer.SetFloat("SFXVolume", (sfx));
+        mixer.SetFloat("MusicVolume", (music));
     }
 
     public void SetMasterVolume(float value)
     {
-        mixer.SetFloat("MasterVolume", LinearToDb(value));
+        mixer.SetFloat("MasterVolume", value);
         PlayerPrefs.SetFloat(MASTER_KEY, value);
     }
     public void SetSfxVolume(float value)
     {
-        mixer.SetFloat("SFXVolume", LinearToDb(value));
+        mixer.SetFloat("SFXVolume", value);
         PlayerPrefs.SetFloat(SFX_KEY, value);
     }
     public void SetMusicVolume(float value)
     {
-        mixer.SetFloat("MusicVolume", LinearToDb(value));
+        mixer.SetFloat("MusicVolume", value);
         PlayerPrefs.SetFloat(MUSIC_KEY, value);
     }
 
